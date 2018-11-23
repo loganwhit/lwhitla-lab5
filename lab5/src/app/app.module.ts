4bcd568ea31b07e0ff2f0756d08e13ae74d7c158
Logan Whitla <lwhitla@uwo.ca>
2018-11-23 08:32:26 +0000

    Added input boxes that show the quantity in basket

--- lab5/src/app/app.module.ts
+++ lab5/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -64,6 +64,7 @@ import { CartComponent } from './cart/cart.component';
     ReactiveFormsModule,
     MatListModule,
     MatDialogModule,
+    FormsModule
    
   ],
   entryComponents:[StartItemComponent, UserItemComponent],
